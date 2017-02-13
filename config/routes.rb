3127e4234a40e86468f70231889b4792feb313dc
jbustillos <bitnami@ubuntu.defaultdomain>
2017-02-13 19:42:09 +0000

    Aplicacion de blog 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :users
   resources :comments
-  resources :posts
+  resources :posts do
+    resource :comments
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
